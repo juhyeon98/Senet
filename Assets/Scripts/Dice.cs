f237169b757080bf7cf1bd8333a9f0f5ae7596e3
juhyeon <griiim134@gmail.com>
2025-07-12 13:12:33 +0900

    feat: add Dice

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+using Effect;
+
 public class Dice : MonoBehaviour
 {
     public EffectData[] effectDatats = new EffectData[6];
---
+++ Assets/Scripts/EffectData.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Effect
+{
+    [CreateAssetMenu(fileName = "EffectData", menuName = "Scriptable Objects/EffectData")]
+    public class EffectData : ScriptableObject
+    {
+        [SerializeField] private uint hp;
+        [SerializeField] private uint mp;
+        [SerializeField] private uint ap;
+        [SerializeField] private uint atk;
+        [SerializeField] private uint def;
+        [SerializeField] private EApplyType applyType;
+
+        public uint HP => hp;
+        public uint MP => mp;
+        public uint AP => ap;
+        public uint ATK => atk;
+        public uint DEF => def;
+        public EApplyType ApplyType => applyType;
+    }
+
+    public enum EApplyType
+    {
+        HP, MP, AP, ATK, DEF, Size
+    };
+}
